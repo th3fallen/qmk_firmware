a447b408bfcbd1b4d553f6a082a1998aeef1dde3
Clark Tomlinson <fallen013@gmail.com>
2020-06-23 13:32:04 -0400

    fix layer tap and macros

--- keyboards/kbdfans/kbd75/keymaps/default/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case OPENCOVERAGE:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) SS_DOWN(X_ZKHK) SS_UP(X_LCTL) SS_UP(X_LSFT) SS_UP(X_ZKHK) SS_DELAY(1000) "~/Sites/When-I-Work/wheniwork-js/reports/coverage/lcov-report/index.html");
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) SS_DOWN(X_ZKHK) SS_UP(X_LCTL) SS_UP(X_LSFT) SS_UP(X_ZKHK) SS_DELAY(300) "open ~/Sites/When-I-Work/wheniwork-js/reports/coverage/lcov-report/index.html" SS_TAP(X_ENTER));
         }
         break;
     }
@@ -31,7 +31,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(
-    MT(MO(1), KC_ESC),   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MPRV,  KC_MPLY,  KC_MNXT,
+    LT(1, KC_ESC),   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MPRV,  KC_MPLY,  KC_MNXT,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
